dcc9ea329eaf1ffd57c3d7360b103fc552d7763f
Wangyx_2002 <wangyx0001@qq.com>
2023-06-09 20:40:05 +0800

    寄存器堆上升沿写入，下降沿读出

--- src/Regfile.v
+++ src/Regfile.v
@@ -1,18 +1,21 @@
 module Regfile206(
-    input           clk,        //时钟信号
-    input[5-1:0]    Rw,         //写入地址
-    input[5-1:0]    Ra,         //A输出地址
-    input[5-1:0]    Rb,         //B输出地址
-    input           WrEn,       //写使能信号
-    input           Jal,        //Jal控制信号
-    input[32-1:0]   busW,       //写入总线
-    output[32-1:0]  busA,       //A输出总线
-    output[32-1:0]  busB);      //B输出总线
+    input               clk,        //时钟信号
+    input[5-1:0]        Rw,         //写入地址
+    input[5-1:0]        Ra,         //A输出地址
+    input[5-1:0]        Rb,         //B输出地址
+    input               WrEn,       //写使能信号
+    input               Jal,        //Jal控制信号
+    input[32-1:0]       busW,       //写入总线
+    output reg[32-1:0]  busA,       //A输出总线
+    output reg[32-1:0]  busB);      //B输出总线
 
     reg[32-1:0] Register[32-1:0];
 
-    assign busA = Register[Ra];
-    assign busB = Register[Rb];
+
+    always @(negedge clk) begin
+        busA <= Register[Ra];
+        busB <= Register[Rb];
+    end
 
     always @(posedge clk) begin
         if(WrEn == 1)begin
